893ff2b473c6df38045e6794e90042d930eb7264
Vignesh R <vignesh.r@convergepoint.com>
2024-11-11 19:36:16 +0530

    npmrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-// import * as React from 'react'
-
+// @ts-ignore
+import {IconChevronRight, IconChevronLeft} from "@convergepoint/sass-icons";
+// @ts-ignore
+import { UiWrapper } from "@convergepoint/sass-ui-components";
+import { PortalCompatProvider } from "@fluentui/react-portal-compat";
+import '@convergepoint/sass-ui-components/dist/style.css';
 
 function App() {
   return (
-    <>
-    <h1>hai</h1>
-    </>
+    <UiWrapper>
+      <PortalCompatProvider>
+    <h1 className="grayColor600">hai</h1>
+    <ul>
+      <li><IconChevronRight/></li>
+      <li><IconChevronLeft/></li>
+    </ul>
+    </PortalCompatProvider>
+    </UiWrapper>
   )
 }
 
