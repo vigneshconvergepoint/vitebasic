19e791ee7ffe51ae63cded0c7aaf499233f57734
Vignesh R <vignesh.r@convergepoint.com>
2024-11-14 12:56:57 +0530

    summa

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import {IconChevronRight, IconChevronLeft} from "@convergepoint/sass-icons";
+// global
+import {IconChevronRight, IconChevronLeft, IconFolderOpen} from "@convergepoint/sass-icons";
 import { UiWrapper } from "@convergepoint/sass-ui-components";
 import { PortalCompatProvider } from "@fluentui/react-portal-compat";
-import '@convergepoint/sass-ui-components/dist/style.css';
+// local
+import "@convergepoint/sass-ui-components/dist/style.css"
 function App() {
   return (
     <UiWrapper>
       <PortalCompatProvider>
-    <h1 className="grayColor600">hai!!</h1>
-    <ul>
-      <li><IconChevronRight/></li>
-      <li><IconChevronLeft/></li>
-    </ul>
+        <h1 className="grayColor600">hai from vite!!</h1>
+        <ul>
+          <li><IconChevronRight/></li>
+          <li><IconChevronLeft/></li>
+          <li><IconFolderOpen/></li>
+        </ul>
     </PortalCompatProvider>
     </UiWrapper>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/App';
 
+const rootElement = document.getElementById('root');
+
+// Create the root container once
+// const root = createRoot(rootElement);
+
+// // Use root.render to update the root with your app
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
