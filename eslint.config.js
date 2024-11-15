e9cb4011abd9e110a12fa3ed633694d7d64c2096
Vignesh R <vignesh.r@convergepoint.com>
2024-11-15 14:54:59 +0530

    build fial

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,25 @@
-import { ESLint } from 'eslint'; // Assuming 'tseslint' is an alias
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint';
+import typescriptParser from '@typescript-eslint/parser';
 
-export default {
-  files: ['**/*.{ts,tsx}'],
-  parser: '@typescript-eslint/parser', // Ensure we're using TypeScript parser
+export default defineConfig({
+  parser: typescriptParser, // Use the TypeScript parser (required for TypeScript)
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  env: {
-    browser: true,
-    es6: true,
-  },
   extends: [
-    'eslint:recommended', // ESLint's base rules
-    'plugin:react/recommended', // React plugin's recommended rules
-    'plugin:prettier/recommended', // Integrate Prettier with ESLint
-    'plugin:@typescript-eslint/recommended', // TypeScript plugin's recommended rules
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Prettier integration
   ],
   plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
+    react: eslintPluginReact,
+    prettier: eslintPluginPrettier,
   },
   rules: {
-    // React Hooks rules
-    ...reactHooks.configs.recommended.rules,
-
-    // React Refresh rule
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': 'error', // Enforce Prettier formatting rules
+    'react/prop-types': 'off',  // Customize based on your project
   },
-  settings: {
-    react: {
-      version: 'detect', // Automatically detect the React version
-    },
-  },
-};
+});
