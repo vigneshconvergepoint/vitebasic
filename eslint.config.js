5f7a58d32b175016a2e680ccd2ebddc0fcf95720
Vignesh R <vignesh.r@convergepoint.com>
2024-11-18 12:59:33 +0530

    eslint fix3

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,28 @@
 // eslint.config.js
-const typescriptParser = require('@typescript-eslint/parser'); // TypeScript parser
-const eslintPluginReact = require('eslint-plugin-react'); // React plugin
-const eslintPluginPrettier = require('eslint-plugin-prettier'); // Prettier plugin
 
-module.exports = {
-  languageOptions: {
-    parser: typescriptParser, // Use the TypeScript parser
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true, // Enable JSX parsing
-      },
+import typescriptParser from '@typescript-eslint/parser'; // Use ES modules
+const config = {
+  parser: typescriptParser,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-  plugins: {
-    react: eslintPluginReact,
-    prettier: eslintPluginPrettier,
-    '@typescript-eslint': 'typescript-eslint', // Ensure you are using the right plugin name
-  },
+  plugins: ['react', 'prettier', '@typescript-eslint'],
   extends: [
-    'eslint:recommended', // Basic ESLint recommendations
-    'plugin:react/recommended', // React specific linting rules
-    'plugin:prettier/recommended', // Prettier integration
-    'plugin:@typescript-eslint/recommended', // TypeScript specific linting rules
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    'prettier/prettier': 'error', // Enforce Prettier formatting
-    'react/prop-types': 'off', // Disable prop-types for TypeScript
-    '@typescript-eslint/no-unused-vars': 'warn', // TypeScript unused vars
-    '@typescript-eslint/explicit-module-boundary-types': 'off', // Optional rule
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
+
+export default config;
