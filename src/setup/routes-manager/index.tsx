62e46a5d24469ee14733b9cfd9246f754b09cfc0
Vignesh R <vignesh.r@convergepoint.com>
2024-11-20 14:48:31 +0530

    navigation done

--- src/setup/routes-manager/index.tsx
+++ src/setup/routes-manager/index.tsx
@@ -3,7 +3,9 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { makeStyles } from "@fluentui/react-components";
 import { IconReport, IconHome } from "@convergepoint/sass-icons";
-import { Navigation } from '@convergepoint/sass-navigation'
+import { Navigation, TopNavBar } from '@convergepoint/sass-navigation';
+import { useMsal } from "@azure/msal-react";
+// local
 
 const MainMenus: any = [
   { menu: "Home", icon: <IconHome />, key: "Home", pageUrl: "/" },
@@ -31,6 +33,14 @@ const useTotalWidth = makeStyles({
 function Licenseportal() {
   const [expanded, setExpanded] = React.useState(false);
   const totalWidth = useTotalWidth();
+  const { instance, accounts } = useMsal();
+
+  const handleLogoutPopup = () => {
+    instance.logoutRedirect({
+      postLogoutRedirectUri: '/',
+    });
+    console.log('User has logged out successfully');
+  };
 
   return (
     <BrowserRouter>
@@ -41,6 +51,11 @@ function Licenseportal() {
         />
 
         <div className={`${expanded ? totalWidth.expand : totalWidth.collapse}`}>
+          <TopNavBar
+            portalName={"License Portal"}
+            userName={accounts[0].name}
+            logout={handleLogoutPopup}
+          />
           <div className="grayBg100 p-1 position-relative">
             <Routes>
               <Route path="/" element={"Home"}></Route>
